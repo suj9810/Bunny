5350f0e76b5068527f67085e81e2d22cb8dcb210
hye-bin0044 <whro080615@gmail.com>
2025-04-23 15:16:29 +0900

    init : Entity 생성

---
+++ src/main/java/sparta/bunny/user/entity/UserEntity.java
@@ -0,0 +1,51 @@
+package sparta.bunny.user.entity;
+
+import org.hibernate.usertype.UserType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+
+	@Column(nullable = false, unique = true)
+	private String userEmail;
+
+	@Column(nullable = true) // 소셜 로그인 사용자의 경우 비밀번호가 없기 때문에
+	private String userPassword;
+
+	@Column(nullable = false)
+	private String nickname;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private UserType userType;
+
+	// // 나중에 추가할 소셜 로그인
+	// @Enumerated(EnumType.STRING)
+	// @Column(nullable = true)
+	// private SocialType socialType;
+
+	@Column(nullable = false)
+	private String userNumber;
+
+	@Column(nullable = false)
+	private Boolean isDeleted = false;
+
+}
