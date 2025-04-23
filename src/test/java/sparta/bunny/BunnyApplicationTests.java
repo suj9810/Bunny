629cc83d8ab5951daaf9a2a542c2b84125d96fc9
uguls <chobozaex@gmail.com>
2025-04-23 12:44:41 +0900

    first commit

---
+++ src/test/java/sparta/bunny/BunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.bunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
