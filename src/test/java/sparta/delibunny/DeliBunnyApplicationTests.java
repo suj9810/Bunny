a066e55e22697fe0147a4cd55431ccdbc075930a
uguls <chobozaex@gmail.com>
2025-04-23 12:05:52 +0900

    first commit

---
+++ src/test/java/sparta/delibunny/DeliBunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.delibunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliBunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
