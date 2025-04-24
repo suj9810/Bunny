f1a4e933b01ed5afe143bb66da3700921ae4e73e
Master <baek011226@naver.com>
2025-04-24 17:25:24 +0900

    feat: 장바구니 엔티티 생성

---
+++ src/main/java/sparta/bunny/domain/cart/entity/Cart.java
@@ -0,0 +1,37 @@
+package sparta.bunny.domain.cart.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import sparta.bunny.domain.cart.dto.CartMenuItem;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@RedisHash("cart")
+public class Cart implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long userId;
+
+    private Long storeId;
+
+    private List<CartMenuItem> menus = new ArrayList<>();
+
+    //장바구니 유효 시간 24시간
+    @TimeToLive
+    private Long ttl = 86400L;
+
+    public Cart(Long userId, Long storeId, List<CartMenuItem> menus) {
+        this.userId = userId;
+        this.storeId = storeId;
+        this.menus = menus;
+    }
+}
