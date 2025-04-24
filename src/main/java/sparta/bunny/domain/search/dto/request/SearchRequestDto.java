9820486ad0670a7000e3f73e136281ba551cc421
haneul02 <redlight02@naver.com>
2025-04-24 22:11:27 +0900

    feat: 가게 등록, 수정, 조회, 폐업 처리 기능 추가
    - OwnerStoreController에 가게 등록, 수정, 조회, 폐업 처리 API 구현
    - OwnerStoreService에 가게 등록, 수정, 폐업 처리 로직 추가
    - StoreRequestDto, StoreResponseDto, StoreStatusDto 수정

--- src/main/java/sparta/bunny/domain/search/dto/request/SearchRequestDto.java
+++ src/main/java/sparta/bunny/domain/search/dto/request/SearchRequestDto.java
@@ -2,9 +2,11 @@ package sparta.bunny.domain.search.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SearchRequestDto {
 
     @NotBlank
