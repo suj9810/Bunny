239b3830d6a57df9a1597d87ef99de952c63cba9
suj9810 <suj9810@naver.com>
2025-04-28 13:31:29 +0900

    Refactor: 메뉴 이미지 예외처리 및 수정 확인

--- src/main/java/sparta/bunny/domain/menu/repository/MenuImageRepository.java
+++ src/main/java/sparta/bunny/domain/menu/repository/MenuImageRepository.java
@@ -3,11 +3,13 @@ package sparta.bunny.domain.menu.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import sparta.bunny.domain.menu.entity.Menu;
 import sparta.bunny.domain.menu.entity.MenuImage;
 
 /**
- * Menu Image Repository
+ * 메뉴 이미지 Repository
  */
 @Repository
 public interface MenuImageRepository extends JpaRepository<MenuImage, Long> {
+	void deleteByMenu(Menu menu);
 }
