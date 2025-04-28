c848d51f61f4f559c9f1d5e1d248af3626a51bfd
uguls <chobozaex@gmail.com>
2025-04-28 16:52:46 +0900

    fix: valid 어노테이션 수정

--- src/main/java/sparta/bunny/domain/review/dto/request/OwnerCommentCreateRequestDto.java
+++ src/main/java/sparta/bunny/domain/review/dto/request/OwnerCommentCreateRequestDto.java
@@ -1,6 +1,7 @@
 package sparta.bunny.domain.review.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OwnerCommentCreateRequestDto {
 
-	@NotBlank
+	@NotNull
 	private Long reviewId;
 	@NotBlank
 	private String content;
--- src/main/java/sparta/bunny/domain/review/dto/request/ReviewCreateRequest.java
+++ src/main/java/sparta/bunny/domain/review/dto/request/ReviewCreateRequest.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +22,12 @@ public class ReviewCreateRequest {
 	@NotBlank
 	private String content;
 
-	@NotBlank
+	@NotNull
+	@Min(1)
+	@Max(5)
 	private Integer rating;
 
-	@NotBlank
+	@NotNull
 	private Long orderId;
 
 	private List<MultipartFile> files;
